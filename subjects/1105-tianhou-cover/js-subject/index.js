faf075b2567686ccf191b5aa9c6bf13388679a22
bjxal <bjxal@139.com>
2014-11-04 18:33:17 +0800

    eileen

--- subjects/1105-tianhou-cover/js-subject/index.js
+++ subjects/1105-tianhou-cover/js-subject/index.js
@@ -2,40 +2,60 @@ $(document).ready(function(){
     //Ðý×ª
     flag = true;
     index = 1;
-    $(".rotate img").addClass("ani");
+    deg = -(360/14);
+    circle_num = 0;
+//    $(".rotate img").addClass("ani");
     interval(index);
     bg_time = setInterval(function(){
+        aa();
+    },2000);
+    function aa(){
         if(flag==true){
-            index++;
+            deg = deg+(360/7);
+            if(deg>360*(circle_num+1)){
+//                deg=-28;
+                circle_num++;
+            }
+            rotate(deg);
             if(index>7) index=1;
             interval(index);
+            index++;
         }
-    },2000);
+    }
     //touch
     $(".link a")
         .on("touchstart",function(e){
             var me = this;
             flag = false;
             var tar = e.target;
-            me.index_e = $(tar).data("index");
+            index = me.index_e = $(tar).data("index");
             interval(me.index_e);
-            var deg = parseInt(me.index_e-1)*50;
+            me.deg = parseInt(me.index_e-1)*(360/7)+circle_num*360;
             $(".rotate img").removeClass("ani").css({
-                webkitTransform:"rotate("+deg+"deg)"
+                webkitTransform:"rotate("+me.deg+"deg)",
+                webkitTransition:"none"
             });
-//            clearInterval(bg_time);
+            clearInterval(bg_time);
         })
         .on("touchmove",function(e){})
         .on("touchend",function(e){
             var me = this;
             flag = true;
-            index = 0;
+            index++;
+            deg = me.deg+25;
             setTimeout(function(){
-                $(".rotate img").addClass("ani");
+                $(".rotate img").css({
+                    webkitTransform:"rotate("+deg+"deg)",
+                    webkitTransition:"all linear 1s"
+                });
             },1000);
+            bg_time = setInterval(function(){
+                aa();
+            },2000);
         });
 });
 function interval(index){
+    //change
     var $bg = $(".bg");
     var $wheel = $(".wheel_bg");
     var $word = $(".word");
@@ -49,4 +69,12 @@ function interval(index){
     $bg.find(".bg_"+index).fadeIn().siblings().css("display","none");
     $wheel.find(".wheel_"+wheel_index).css("display","block").siblings().css("display","none");
     $word.find(".word_"+index).css("display","block").siblings().css("display","none");
+}
+function rotate(deg){
+    //rotate
+    var rotate = $(".rotate img");
+    rotate.css({
+        webkitTransform:"rotate("+deg+"deg)",
+        webkitTransition:"all linear 2s"
+    });
 }
